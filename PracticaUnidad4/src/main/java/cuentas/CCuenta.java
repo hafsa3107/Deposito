86649a8f2dc34e3751a9144a3b3ea8ea71f03854
Hafsa <hafsa3107@gmail.com>
2025-02-27 12:17:30 +0000

    Refactorizacion: mover clases al paquete cuentas

---
+++ PracticaUnidad4/src/main/java/cuentas/CCuenta.java
@@ -0,0 +1,77 @@
+package cuentas;
+
+/**
+ *
+ * @author Hafsa
+ */
+
+public class CCuenta {
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta() {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        this.nombre = nom;
+        this.cuenta = cue;
+        this.saldo = sal;
+        this.tipoInterés = tipo;
+    }
+
+    public double estado() {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        saldo += cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No hay suficiente saldo");
+        }
+        saldo -= cantidad;
+    }
+
+    // Métodos Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInterés;
+    }
+
+    public void setTipoInteres(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+}
